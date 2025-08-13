e0b81a8b21ab2f87017f75fd6762381c8333a35d
koto-1207 <koto0429@gmail.com>
2025-08-13 12:35:08 +0900

    make beatiful_kuku

--- b_3_beatiful_kuku.py
+++ b_3_beatiful_kuku.py
@@ -8,7 +8,7 @@ retu = 9
 for dan in range(1, dan + 1):
     for i in range(1, retu + 1):
         product = dan * i
-        calc = f"{dan} X {i} = {product} |"
-        
-        print(calc, end=" ")
+        calc = f"{dan} X {i} = {product:2}"
+
+        print(calc, end=" | ")
     print()
