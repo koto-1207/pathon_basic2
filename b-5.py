d0441229761051a3921a83700017fd01b4bc10ac
koto-1207 <koto0429@gmail.com>
2025-08-13 17:34:01 +0900

    ]平均値作成中

---
+++ b-5.py
@@ -0,0 +1,49 @@
+# 合計値
+from unittest import result
+
+
+def total_num():
+    total = 0
+    for i in numbers:
+        total += i
+    return total
+
+
+# 最大値
+def max_num():
+    max = numbers[0]
+    for i in range(1, len(numbers)):
+        if numbers[i] > max:
+            max = numbers[i]
+    return max
+
+
+# 最小値
+def min_num():
+    min = numbers[0]
+    for i in range(1, len(numbers)):
+        if numbers[i] < min:
+            min = numbers[i]
+    return min
+
+
+# 平均値
+def avg_num():
+    total = 0
+    for i in numbers:
+        result = total + i
+
+    avg_value = result // len(numbers)
+    return avg_value
+
+
+data = input("データを入力してください（スペース区切り）：")
+numbers = []
+for item in data.split():
+    numbers.append(int(item))
+
+
+print(f"合計値: {total_num()}")
+print(f"最大値: {max_num()}")
+print(f"最小値: {min_num()}")
+print(f"平均値: {avg_num()}")
